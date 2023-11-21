522c5b446a91730baf6c04c54518f51d010ba11f
bingli-borland <1042996632@qq.com>
2023-11-21 23:09:51 +0800

    区别于undertow镜像配置

--- demo-request/src/main/java/com/bingli/test/DataServer.java
+++ demo-request/src/main/java/com/bingli/test/DataServer.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.HashMap;
 
 //@FeignClient(name = "demo-data", url = "http://localhost:12001")
-@FeignClient(name = "demo-data", url = "http://demo-data.myes:12001")
+@FeignClient(name = "demo-data", url = "http://demo-data-bes.myes:12001")
 public interface DataServer {
     @GetMapping("data")
     public ResponseVO<HashMap> data();
