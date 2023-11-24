06155b39bbd2f0d0c256443308192ced7ee48b40
bingli-borland <1042996632@qq.com>
2023-11-24 13:04:15 +0800

    动态化后端地址

--- demo-request/src/main/java/com/bingli/test/RequestController.java
+++ demo-request/src/main/java/com/bingli/test/RequestController.java
@@ -2,6 +2,7 @@ package com.bingli.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +18,10 @@ public class RequestController {
     private RestTemplate restTemplate;
 
     @GetMapping("request")
-    public ResponseVO data(){
-        return restTemplate.getForObject("http://demo-data-bes.myes:12001/data", ResponseVO.class);
+    public ResponseVO data(@RequestParam("dataAddress") String address){
+        if (address == null) {
+            address = "demo-data-bes.myes";
+        }
+        return restTemplate.getForObject("http://" + address + ":12001/data", ResponseVO.class);
     }
 }
